ef8db8706ea19259f3ec88e10aec1191096ab008
yuekesoft <48050385+yuekesoft@users.noreply.github.com>
2022-10-05 23:31:08 +0800

    Update sidebar.js

--- docs/.vitepress/sidebar.js
+++ docs/.vitepress/sidebar.js
@@ -18,6 +18,7 @@ const sidebar =  {
         { text: '注册服务端', link: '/guide/regserver' },
         { text: '软件登陆', link: '/guide/login' },
         { text: '添加会员信息', link: '/guide/addmember' },
+        { text: '会议场次设置', link: '/guide/meetingset' },
         { text: '刷卡积分签到', link: '/guide/signin' },
         { text: '客户积分签到统计报表', link: '/guide/signlog' },
         { text: '短信群发', link: '/guide/smssend' }      
