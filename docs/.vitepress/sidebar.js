3934661a8aa36b1b5ff7c58622623809c21927c6
yuekesoft <1961339@qq.com>
2022-10-06 22:02:03 +0800

    add netfail useridinvalid

--- docs/.vitepress/sidebar.js
+++ docs/.vitepress/sidebar.js
@@ -20,6 +20,7 @@ const sidebar =  {
         { text: '添加会员信息', link: '/guide/addmember' },
         { text: '会议场次设置', link: '/guide/meetingset' },
         { text: '刷卡积分签到', link: '/guide/signin' },
+        { text: '手工会员积分添加', link: '/guide/handpoint' },
         { text: '客户积分签到统计报表', link: '/guide/signlog' },
         { text: '短信群发', link: '/guide/smssend' }      
       ]
@@ -27,15 +28,22 @@ const sidebar =  {
     {
       text: '高级功能',
       collapsible: true,
-      //collapsed: true,
+      collapsed: true,
       items:[
         { text: '会员等级设置', link: '/guide/memberlevel' },
         { text: '兑换礼品设置', link: '/guide/giftset' },
-        { text: '手工会员积分添加', link: '/guide/handpoint' },
         { text: '系统参数设置', link: '/guide/sysset' },
         { text: '小票打印格式设置', link: '/guide/printset' }
       ]
-    }
+    },
+		{
+      text: '常见问题',
+      collapsible: true,
+      items:[
+        { text: '用户账号无效', link: '/guide/useridinvalid' },
+        { text: '连接服务器失败', link: '/guide/netfail' }
+      ]
+    } 
   ]
 }
 
